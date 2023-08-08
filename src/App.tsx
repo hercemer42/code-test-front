2a448fe0bb980a3d704fd467529a373353035cdc
Jean-Baptiste Bellet <jb.bellet@gmail.com>
2023-08-08 18:14:42 +0200

    Change title, center it and add some padding to questions section below

--- src/App.tsx
+++ src/App.tsx
@@ -15,8 +15,10 @@ function App() {
   return (
     <div className="App px-4 sm:px-6 md:px-8">
       <div className="max-w-5xl mx-auto pt-20 sm:pt-24 lg:pt-32">
-        <h1 className="text-3xl font-bold">Teste tes connaissances</h1>
-        <p>{JSON.stringify(questions)}</p>
+        <h1 className="text-3xl font-bold text-center">
+          Teste tes connaissances sur l'environnement
+        </h1>
+        <div className="pt-20">{JSON.stringify(questions)}</div>
       </div>
     </div>
   );
