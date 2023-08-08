97a747394edac8c9470f6c779602228816d81639
Jean-Baptiste Bellet <jb.bellet@gmail.com>
2023-08-08 19:22:27 +0200

    Add soft grey border to body

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
   }, []);
 
   return (
-    <div className="App px-4 sm:px-6 md:px-8">
+    <div className="App px-4 sm:px-6 md:px-8 bg-gray-100">
       <div className="max-w-5xl mx-auto pt-20 sm:pt-24 lg:pt-32">
         <h1 className="text-3xl font-bold text-center">
           Teste tes connaissances sur l'environnement
