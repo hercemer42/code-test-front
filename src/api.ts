5b6a2e8eada8cec1d88d3a9ba03438246f111af6
Jean-Baptiste Bellet <jb.bellet@gmail.com>
2023-08-08 19:06:15 +0200

    Use `filterByDifficulties` method to get correct array in API
    Sort the questions _before_ filtering in order to have randomized question. No need to slice because of `DIFFICULTIES` parameter

--- src/api.ts
+++ src/api.ts
@@ -1,9 +1,28 @@
+import { filterByDifficulties } from "./api/api.utils";
+
+const DIFFICULTIES: Array<{ difficulty: Difficulty; count: number }> = [
+  {
+    difficulty: "easy",
+    count: 2,
+  },
+  {
+    difficulty: "medium",
+    count: 2,
+  },
+  {
+    difficulty: "hard",
+    count: 1,
+  },
+];
+
 class API {
   static async getFiveRandomizedQuestions(): Promise<Array<Question>> {
     const response = await fetch("/environment_questions");
     const questions = await response.json();
-    questions.sort(() => Math.random() - Math.random()).slice(0, 5);
-    return questions;
+    return filterByDifficulties(
+      questions.sort(() => Math.random() - Math.random()),
+      DIFFICULTIES
+    );
   }
 }
 
