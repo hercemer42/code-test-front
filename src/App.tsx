4d8e528da2c3b90732374ae28526c006145b2680
Jean-Baptiste Bellet <jb.bellet@gmail.com>
2023-08-08 17:49:10 +0200

    Create basic margin around application to not stick browser border

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,11 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <h1>Teste tes connaissances</h1>
-      <p>{JSON.stringify(questions)}</p>
+    <div className="App px-4 sm:px-6 md:px-8">
+      <div className="max-w-5xl mx-auto pt-20 sm:pt-24 lg:pt-32">
+        <h1 className="text-3xl font-bold">Teste tes connaissances</h1>
+        <p>{JSON.stringify(questions)}</p>
+      </div>
     </div>
   );
 }
