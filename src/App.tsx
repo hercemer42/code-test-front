2c3fb40d2b18f4e7c1a037f5b5f7feead5c70e0a
Jean-Baptiste Bellet <jb.bellet@gmail.com>
2023-08-09 13:47:03 +0200

    Create a route `/environnement` to the Environnement element
    `/` fallback to that page too

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
+import { Route, BrowserRouter, Routes } from "react-router-dom";
 import "./App.css";
-import Quizz from "./containers/Quizz";
+import Environnement from "./routes/Environnement";
 
 function App() {
   return (
-    <div className="App px-4 sm:px-6 md:px-8 bg-gray-100">
-      <div className="max-w-5xl mx-auto pt-20 pb-20 sm:pt-24 lg:pt-32">
-        <h1 className="text-3xl font-bold text-center">
-          Teste tes connaissances sur l'environnement
-        </h1>
-        <div className="pt-20">
-          <Quizz />
+    <BrowserRouter>
+      <div className="App px-4 sm:px-6 md:px-8 bg-gray-100">
+        <div className="max-w-5xl mx-auto pt-20 pb-20 sm:pt-24 lg:pt-32">
+          <Routes>
+            <Route path="/" element={<Environnement />}>
+              <Route path="environnement" element={<Environnement />} />
+            </Route>
+          </Routes>
         </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/routes/Environnement.tsx
@@ -0,0 +1,16 @@
+import Quizz from "../containers/Quizz";
+
+const Environnement = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-center">
+        Teste tes connaissances sur l'environnement
+      </h1>
+      <div className="pt-20">
+        <Quizz />
+      </div>
+    </div>
+  );
+};
+
+export default Environnement;
