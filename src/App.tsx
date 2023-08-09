6393e076801c8a9f4f2cca68325be2410bcd1102
Jean-Baptiste Bellet <jb.bellet@gmail.com>
2023-08-09 14:13:19 +0200

    Create a NavBat

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { Route, BrowserRouter, Routes } from "react-router-dom";
 import "./App.css";
 import Environnement from "./routes/Environnement";
 import Mitigation from "./routes/Mitigation";
+import NavBar from "./containers/NavBar";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App px-4 sm:px-6 md:px-8 bg-gray-100">
         <div className="max-w-5xl mx-auto pt-20 pb-20 sm:pt-24 lg:pt-32">
+          <NavBar />
           <Routes>
             <Route path="/" element={<Environnement />}>
               <Route path="environnement" element={<Environnement />} />
