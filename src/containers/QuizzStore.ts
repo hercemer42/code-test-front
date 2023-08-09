019b526e93f5e8e9a4ede16c736178feddb35e1b
Jean-Baptiste Bellet <jb.bellet@gmail.com>
2023-08-09 14:36:36 +0200

    Parametrize score depending on Quizz type

--- src/containers/QuizzStore.ts
+++ src/containers/QuizzStore.ts
@@ -15,31 +15,39 @@ const QuizzStore = new Store<QuizzStore>({
 
 export default QuizzStore;
 
-export const fetchQuestions = createAsyncAction(
-  async ({ type }: { type: QuizzType }) => {
-    const questions = await API.getFiveRandomizedQuestions(type);
-    return successResult({ questions: questions });
-  },
-  {
-    postActionHook: ({ result }) => {
-      QuizzStore.update((s) => {
-        s.environment.questions = result.payload?.questions as Array<Question>;
-      });
-    },
-  }
-);
+const fetchQuestionsAction = async ({ type }: { type: QuizzType }) => {
+  const questions = await API.getFiveRandomizedQuestions(type);
+  return successResult({ type: type, questions: questions });
+};
 
-export const submitAnswers = createAsyncAction(
-  async ({ type, answers }: { type: QuizzType; answers: Array<ApiAnswer> }) => {
-    const score = await API.submitAnswers(type, answers);
-    return successResult({ score: score });
+export const fetchQuestions = createAsyncAction(fetchQuestionsAction, {
+  postActionHook: ({ result }) => {
+    const questions = result.payload?.questions as Array<Question>;
+    const type = result.payload?.type as QuizzType;
+    QuizzStore.update((s) => {
+      s[type].questions = questions;
+    });
   },
-  {
-    postActionHook: ({ result }) => {
-      QuizzStore.update((s) => {
-        s.isSubmitting = false;
-        s.environment.score = result.payload?.score as number;
-      });
-    },
-  }
-);
+});
+
+const submitAnswersAction = async ({
+  type,
+  answers,
+}: {
+  type: QuizzType;
+  answers: Array<ApiAnswer>;
+}) => {
+  const score = await API.submitAnswers(type, answers);
+  return successResult({ type: type, score: score });
+};
+
+export const submitAnswers = createAsyncAction(submitAnswersAction, {
+  postActionHook: ({ result }) => {
+    const score = result.payload?.score as number;
+    const type = result.payload?.type as QuizzType;
+    QuizzStore.update((s) => {
+      s.isSubmitting = false;
+      s[type].score = score;
+    });
+  },
+});
